2f53d0e87984f22a1f2e1c334825f85be6c1ebc9
shrish tripathi <shrish191@users.noreply.huggingface.co>
2025-05-04 19:10:07 +0000

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -8,8 +8,11 @@ def get_classification_report():
     true_labels = df["label"].tolist()
 
     # Load tokenizer and model
-    tokenizer = AutoTokenizer.from_pretrained("Shrish/mbert-sentiment")
-    model = TFAutoModelForSequenceClassification.from_pretrained("Shrish/mbert-sentiment")
+    #tokenizer = AutoTokenizer.from_pretrained("Shrish/mbert-sentiment")
+    #model = TFAutoModelForSequenceClassification.from_pretrained("Shrish/mbert-sentiment")
+    fallback_model_name = "cardiffnlp/twitter-roberta-base-sentiment"
+    fallback_tokenizer = AutoTokenizer.from_pretrained(fallback_model_name)
+    fallback_model = AutoModelForSequenceClassification.from_pretrained(fallback_model_name)
 
     # Tokenize and predict
     inputs = tokenizer(texts, padding=True, truncation=True, return_tensors="tf")
